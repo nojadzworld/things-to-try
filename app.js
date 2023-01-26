15b4ed530a321fc83497988ddb0026499b673424
DaJon Ewing <dajone37@gmail.com>
2023-01-26 12:53:00 -0800

    understanding functions

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
- let userName = prompt('What is your name?');
- document.write ('Hello..' +  userName +'!' 
- );
 
+    function yourname(){
+let userName = prompt('What is your name?');
+    document.write ('Hello.. ' +  userName +'!' 
+
+    )}
+function movie(){
 let movieAnswer =prompt('What is my favorite movie?');
 
-let mymovieanswer = 'Home Alone';
-
-if (movieAnswer == mymovieanswer){
-    document.write('You got it!')
+if (movieAnswer == 'Home Alone'){
+    document.write(' You got it!   ')
 }
-else {document.write("Nope. It is" + mymovieanswer)}
+else {document.write(" Nope. It is  " + mymovieanswer)}
+}
 
+function age(){
 let myage =prompt('How old do I look based on my profile picture');
 
 let myageanswer = 23;
 
 if (myage == myageanswer) {
-    document.write('Wow you are correct!')
+    document.write('   Wow you are correct!')
 }
 
 else if (myage > myageanswer) {
-    document.write('Chill!! Im not that old')
+    document.write('   Chill!! Im not that old')
 }
 else if (myage < myageanswer){
-    document.write('Do i look like a baby?')
+    document.write('   Do i look like a baby?')
 }
 
 else {
-    document.write ('Impossible...')
+    document.write ('  How? Impossible...')
 }
+}
+  yourname(); 
+  movie();
+  age();
+
+
+ 
